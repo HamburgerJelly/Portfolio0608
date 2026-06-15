689c4e73d36b6357869a47ab97aafc7e51bafded
HamburgerJelly <kmj74450668@gmail.com>
2026-06-15 15:40:57 +0900

    Add files via upload

---
+++ js/gallery-data.js
@@ -0,0 +1,8 @@
+const galleryData = [
+  {id:1,title:"쇼르모트 브랜드",category:"DESIGN",categoryName:"디자인",date:"2026.06.01",image:"images/gallery/photo01.png",desc:"초콜릿 브랜드 쇼르모트를 디자인하였습니다."},
+  {id:2,title:"스킨푸드 브랜드 일러스트",category:"ILLUSTRATION",categoryName:"일러스트",date:"2026.06.02",image:"images/gallery/photo02.jpeg",desc:"스킨푸드 브랜드를 컨셉으로 일러스트를 작업하였습니다."},
+  {id:3,title:"신라대학교 게임콘텐츠디자인학과 홍보 영상",category:"VIDEO",categoryName:"영상",date:"2026.06.03",image:"images/gallery/photo03.png",desc:"신라대학교 게임콘텐츠디자인학과 홍보 영상을 애니메이션으로 제작하였습니다."},
+  {id:4,title:"<내 모자 어디 갔을까?> 타이포그래피 포스터",category:"DESIGN",categoryName:"디자인",date:"2026.06.04",image:"images/gallery/photo04.png",desc:"존 클라센의 동화책 <내 모자 어디 갔을까?>를 주제로 타이포그래피 포스터를 제작하였습니다."},
+  {id:5,title:"단편 애니메이션 <작은 크리스마스>",category:"VIDEO",categoryName:"영상",date:"2026.06.05",image:"images/gallery/photo05.png",desc:"단편 애니메이션인 <작은 크리스마스>를 제작하였습니다."},
+  {id:6,title:"<눈 케이크> 게임",category:"GAME",categoryName:"게임",date:"2026.06.06",image:"images/gallery/photo06.png",desc:"<눈 케이크>라는 게임을 개발 중입니다."}
+];
