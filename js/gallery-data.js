eee2051c66c828fba35f85674fe9597d4352c480
HamburgerJelly <kmj74450668@gmail.com>
2026-06-21 21:41:13 +0900

    Add files via upload

---
+++ js/gallery-data.js
@@ -0,0 +1,8 @@
+const galleryData = [
+  {id:1,title:"'Snow Cake' 포스터",category:"poster",categoryName:"포스터",date:"2025.05.14",image:"images/gallery/photo01.png",desc:"'Snow Cake'의 초반 스토리를 담은 포스터를 제작하였습니다."},
+  {id:2,title:"<물에 잠긴 도서관> 일러스트",category:"illust",categoryName:"일러스트레이션",date:"2025.09.08",image:"images/gallery/photo02.jpg",desc:"배경은 신라대학교 도서관을 참고하였습니다."},
+  {id:3,title:"'예랑가랑' 포스터",category:"poster",categoryName:"포스터",date:"2025.11.24",image:"images/gallery/photo03.png",desc:"유튜버 '예랑가랑'을 주제로 포스터를 제작하였습니다."},
+  {id:4,title:"<두 마녀의 하루> 포스터",category:"poster",categoryName:"포스터",date:"2025.12.15",image:"images/gallery/photo04.png",desc:"서로 닮은 형태의 사물을 연결하고 재해석하여, '두 마녀의 하루'라는 스토리로 표현해 보았습니다."},
+  {id:5,title:"'스킨푸드' 일러스트",category:"illust",categoryName:"일러스트",date:"2026.05.11",image:"images/gallery/photo05.jpeg",desc:"'스킨푸드' 브랜드를 주제로 일러스트를 제작하였습니다."},
+  {id:6,title:"<내 모자 어디 갔을까?> 포스터",category:"poster",categoryName:"포스터",date:"2026.05.26",image:"images/gallery/photo06.png",desc:"동화책 <내 모자 어디 갔을까?>를 주제로 타이포그래피 포스터를 제작하였습니다."}
+];
